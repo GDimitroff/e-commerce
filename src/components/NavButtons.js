cbfbc0ad0bb00f1c51bc49b6949e794d9e135a5f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-06 12:40:46 +0300

    Add logo, navbar, navbar buttons

---
+++ src/components/NavButtons.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from 'react-icons/fa';
+
+const CartButtons = () => {
+  return (
+    <Wrapper className="nav-buttons-wrapper">
+      <Link to="cart" className="cart-btn">
+        Cart
+        <span className="cart-container">
+          <FaShoppingCart />
+          <span className="cart-value">12</span>
+        </span>
+      </Link>
+      <button type="button" className="auth-btn">
+        Login <FaUserPlus />
+      </button>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  gap: 12px;
+
+  .cart-btn {
+    font-size: 1.6rem;
+    letter-spacing: var(--small-spacing);
+    color: var(--color-grey-6);
+    display: flex;
+    align-items: center;
+    padding: 0.6rem 0;
+    border-bottom: 2px solid transparent;
+    transition: var(--transition);
+
+    &:hover {
+      border-bottom: 2px solid var(--color-primary-5);
+    }
+  }
+
+  .cart-container {
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    svg {
+      width: 2.2rem;
+      height: 2.2rem;
+      margin-left: 5px;
+    }
+  }
+
+  .cart-value {
+    position: absolute;
+    top: -12px;
+    right: -14px;
+    background: var(--color-primary-5);
+    width: 16px;
+    height: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 1.2rem;
+    color: var(--color-grey-1);
+    font-weight: 700;
+    padding: 12px;
+  }
+
+  .auth-btn {
+    font-family: inherit;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    background: transparent;
+    border-color: transparent;
+    color: var(--color-grey-6);
+    letter-spacing: var(--small-spacing);
+    padding: 0.6rem 0;
+    border-bottom: 2px solid transparent;
+    transition: var(--transition);
+    cursor: pointer;
+
+    &:hover {
+      border-bottom: 2px solid var(--color-primary-5);
+    }
+
+    svg {
+      width: 2.2rem;
+      height: 2.2rem;
+      margin-left: 5px;
+    }
+  }
+`;
+export default CartButtons;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,117 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/logo.png';
+import { FaBars } from 'react-icons/fa';
+import { links } from '../utils/constants';
+import NavButtons from './NavButtons';
+
+const Nav = () => {
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="The Casa Décor" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <NavButtons />
+      </div>
+    </NavContainer>
+  );
 };
 
-export default Navbar;
+const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0f172a;
+
+  .nav-center {
+    width: 100vw;
+    padding: 2rem;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+      width: 200px;
+    }
+  }
+
+  .nav-toggle {
+    background: transparent;
+    border: transparent;
+    color: var(--color-primary-5);
+    cursor: pointer;
+
+    svg {
+      font-size: 2.6rem;
+    }
+  }
+
+  .nav-links {
+    display: none;
+  }
+
+  .nav-buttons-wrapper {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      gap: 18px;
+
+      a {
+        color: var(--color-grey-6);
+        font-size: 1.6rem;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: var(--medium-spacing);
+        padding: 0.6rem;
+        border-bottom: 2px solid transparent;
+        transition: var(--transition);
+
+        &:hover {
+          border-bottom: 2px solid var(--color-primary-5);
+        }
+      }
+    }
+
+    .nav-buttons-wrapper {
+      display: grid;
+    }
+  }
+`;
+
+export default Nav;
