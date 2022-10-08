b73d89f5ed98614391c62561483be4bc9e69ef6f
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-08 14:34:06 +0300

    Fix gallery

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,7 +1,76 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 
-const ProductImages = () => {
-  return <h4>product images</h4>;
+const ProductImages = ({ images }) => {
+  const [main, setMain] = useState(images[0]);
+
+  return (
+    <Wrapper>
+      <img src={main.url} alt="Primary" className="main" />
+      <div className="gallery">
+        {images.map((image, index) => {
+          return (
+            <img
+              className={`${image.url === main.url ? 'active' : ''}`}
+              src={image.url}
+              alt={image.filename}
+              key={image.id}
+              onClick={() => setMain(images[index])}
+            />
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    object-fit: cover;
+  }
+
+  .gallery {
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    column-gap: 1rem;
+
+    img {
+      height: 100px;
+      cursor: pointer;
+    }
+  }
+
+  .active {
+    box-shadow: 0px 0px 0px 2px var(--color-grey-1);
+  }
+
+  @media (max-width: 576px) {
+    .main {
+      height: 300px;
+    }
+
+    .gallery {
+      img {
+        height: 50px;
+      }
+    }
+  }
+
+  @media (min-width: 992px) {
+    .main {
+      height: 450px;
+    }
+
+    .gallery {
+      img {
+        height: 75px;
+      }
+    }
+  }
+`;
 
 export default ProductImages;
