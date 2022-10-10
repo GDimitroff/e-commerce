b15c298fe7d4db7362b372549009c192c58c3421
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-10 18:43:24 +0300

    Fix cart reducer

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -10,26 +10,73 @@ const cartReducer = (state, action) => {
   switch (action.type) {
     case ADD_TO_CART: {
       const { id, color, amount, product } = action.payload;
-      const existingItem = state.cart.find((item) => item.id === id + color);
+      const existingGroupItems = state.cart.find((item) => item.id === id);
 
-      if (existingItem) {
-        const newCart = state.cart.map((cartItem) => {
-          if (cartItem.id === id + color) {
-            let newAmount = cartItem.amount + amount;
-            if (newAmount > cartItem.max) {
-              newAmount = cartItem.max;
-            }
+      if (existingGroupItems) {
+        const existingItem = state.cart.find(
+          (item) => item.uniqueId === id + color
+        );
 
-            return { ...cartItem, amount: newAmount };
-          } else {
-            return cartItem;
+        const totalAmountInCart = state.cart.reduce((total, item) => {
+          if (item.id === id) {
+            total += item.amount;
           }
-        });
+
+          return total;
+        }, 0);
+
+        let newCart;
+        if (existingItem) {
+          const availableSpace =
+            product.stock - (totalAmountInCart - existingItem.amount);
+
+          if (availableSpace === 0) {
+            return { ...state };
+          }
+
+          newCart = state.cart.map((item) => {
+            if (item.uniqueId === id + color) {
+              let newAmount = item.amount + amount;
+              if (newAmount > availableSpace) {
+                newAmount = availableSpace;
+              }
+
+              return { ...item, amount: newAmount };
+            } else {
+              return { ...item };
+            }
+          });
+        } else {
+          const availableSpace = product.stock - totalAmountInCart;
+          let newAmount = amount;
+
+          if (availableSpace === 0) {
+            return { ...state };
+          }
+
+          if (newAmount > availableSpace) {
+            newAmount = availableSpace;
+          }
+
+          let newItem = {
+            id,
+            uniqueId: id + color,
+            name: product.name,
+            color,
+            amount: newAmount,
+            image: product.images[0].url,
+            price: product.price,
+            max: product.stock,
+          };
+
+          newCart = [...state.cart, newItem];
+        }
 
         return { ...state, cart: newCart };
       } else {
         let newItem = {
-          id: id + color,
+          id,
+          uniqueId: id + color,
           name: product.name,
           color,
           amount,
