e5bdf3a03c9f82ec5359e50fcfabbba2679acdb2
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-10 12:39:19 +0300

    Add loading state in products list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,15 @@
 import { useFilterContext } from '../context/FilterContext';
+import { useProductsContext } from '../context/ProductsContext';
 import GridView from './GridView';
 import ListView from './ListView';
 
 const ProductList = () => {
+  const { productsLoading: loading } = useProductsContext();
   const { filteredProducts: products, gridView } = useFilterContext();
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
 
   if (products.length < 1) {
     return <h4>Sorry, no products matched your search...</h4>;
