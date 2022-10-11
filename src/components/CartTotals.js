7b6a1142dd51653bd5c6e07cc1536ca7e25a3c2d
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-11 12:10:01 +0300

    Add cart totals

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -1,5 +1,62 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { useCartContext } from '../context/CartContext';
+import { formatPrice } from '../utils/helpers';
+
 const CartTotals = () => {
-  return <h4>cart totals</h4>;
+  const { totalAmount, shippingFee } = useCartContext();
+
+  return (
+    <Wrapper>
+      <div>
+        <article>
+          <h5>
+            Subtotal: <span>{formatPrice(totalAmount)}</span>
+          </h5>
+          <h5>
+            Shipping fee: <span>{formatPrice(shippingFee)}</span>
+          </h5>
+          <hr />
+          <h4>
+            Order total: <span>{formatPrice(totalAmount + shippingFee)}</span>
+          </h4>
+        </article>
+        <Link to="/checkout" className="btn">
+          Proceed to checkout
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+
+  article {
+    border: 1px solid var(--color-grey-8);
+    border-radius: var(--radius);
+    padding: 3rem;
+  }
+
+  h5 {
+    color: var(--color-grey-4);
+    display: grid;
+    grid-template-columns: 200px 1fr;
+  }
+
+  @media (min-width: 776px) {
+    justify-content: flex-end;
+  }
+
+  .btn {
+    width: 100%;
+    margin-top: 1rem;
+    text-align: center;
+    font-weight: 700;
+  }
+`;
 
 export default CartTotals;
