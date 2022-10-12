a6f1103f0c2ddb5a02eb21bc0b2571ea97c72bb4
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-10-12 12:19:18 +0300

    Add checkout placeholder

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -6,15 +6,39 @@ const CheckoutPage = () => {
   return (
     <main>
       <Breadcrumbs title="Checkout" />
-      <Wrapper>
-        <h1>checkout here</h1>
+      <Wrapper className="section">
+        <div className="checkout">
+          <h1>Checkout</h1>
+          <p>Stripe integration will be here someday in the future...</p>
+        </div>
       </Wrapper>
     </main>
   );
 };
 
 const Wrapper = styled.div`
-  min-height: calc(100vh - (5vh + 17rem));
+  min-height: calc(
+    100vh -
+      (
+        var(--navbar-height) + var(--breadcrumbs-height) + var(--footer-height) +
+          10rem
+      )
+  );
+
+  display: grid;
+  place-items: center;
+
+  .checkout {
+    color: var(--color-grey-9);
+    padding: 4rem;
+    text-align: center;
+    background-color: var(--color-green-light);
+    border-radius: var(--radius);
+
+    h1 {
+      margin-bottom: 2rem;
+    }
+  }
 `;
 
 export default CheckoutPage;
